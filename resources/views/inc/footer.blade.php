975f95bc9f29270111632c979e36a7dd6751c12a
dikum <amukida@gmail.com>
2020-06-23 23:44:44 +0100

    Create footer.blade.php

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,83 @@
+
+
+<!--[if lt IE 10]>
+    <div class="ie-warning">
+        <h1>Warning!!</h1>
+        <p>You are using an outdated version of Internet Explorer, please upgrade
+            <br/>to any of the following web browsers to access this website.
+        </p>
+        <div class="iew-container">
+            <ul class="iew-download">
+                <li>
+                    <a href="http://www.google.com/chrome/">
+                        <img src="./assets/images/browser/chrome.png" alt="Chrome">
+                        <div>Chrome</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.mozilla.org/en-US/firefox/new/">
+                        <img src="./assets/images/browser/firefox.png" alt="Firefox">
+                        <div>Firefox</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.opera.com">
+                        <img src="./assets/images/browser/opera.png" alt="Opera">
+                        <div>Opera</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.apple.com/safari/">
+                        <img src="./assets/images/browser/safari.png" alt="Safari">
+                        <div>Safari</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://windows.microsoft.com/en-us/internet-explorer/download-ie">
+                        <img src="./assets/images/browser/ie.png" alt="">
+                        <div>IE (9 & above)</div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <p>Sorry for the inconvenience!</p>
+    </div>
+<![endif]-->
+
+
+<script data-cfasync="false" src="./assets/js/email-decode.min.js"></script><script type="93fd9015cca482e27f619fc4-text/javascript" src="./assets/js/jquery.min.js"></script>
+<script type="93fd9015cca482e27f619fc4-text/javascript" src="./assets/js/jquery-ui.min.js"></script>
+<script type="93fd9015cca482e27f619fc4-text/javascript" src="./assets/js/popper.min.js"></script>
+<script type="93fd9015cca482e27f619fc4-text/javascript" src="./assets/js/bootstrap.min.js"></script>
+
+<script src="./assets/js/waves.min.js" type="93fd9015cca482e27f619fc4-text/javascript"></script>
+
+<script type="93fd9015cca482e27f619fc4-text/javascript" src="./assets/js/jquery.slimscroll.js"></script>
+
+<script src="./assets/js/jquery.flot.js" type="93fd9015cca482e27f619fc4-text/javascript"></script>
+<script src="./assets/js/jquery.flot.categories.js" type="93fd9015cca482e27f619fc4-text/javascript"></script>
+<script src="./assets/js/curvedLines.js" type="93fd9015cca482e27f619fc4-text/javascript"></script>
+<script src="./assets/js/jquery.flot.tooltip.min.js" type="93fd9015cca482e27f619fc4-text/javascript"></script>
+
+<script src="./assets/js/chartist.js" type="93fd9015cca482e27f619fc4-text/javascript"></script>
+
+<script src="./assets/js/amcharts.js" type="93fd9015cca482e27f619fc4-text/javascript"></script>
+<script src="./assets/js/serial.js" type="93fd9015cca482e27f619fc4-text/javascript"></script>
+<script src="./assets/js/light.js" type="93fd9015cca482e27f619fc4-text/javascript"></script>
+
+<script src="./assets/js/pcoded.min.js" type="93fd9015cca482e27f619fc4-text/javascript"></script>
+<script src="./assets/js/vertical-layout.min.js" type="93fd9015cca482e27f619fc4-text/javascript"></script>
+<script type="93fd9015cca482e27f619fc4-text/javascript" src="./assets/js/custom-dashboard.min.js"></script>
+<script type="93fd9015cca482e27f619fc4-text/javascript" src="./assets/js/script.min.js"></script>
+<script type="93fd9015cca482e27f619fc4-text/javascript" src="./assets/js/clock.js"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13" type="93fd9015cca482e27f619fc4-text/javascript"></script>
+<script type="93fd9015cca482e27f619fc4-text/javascript">
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+<script src="https://ajax.cloudflare.com/cdn-cgi/scripts/7089c43e/cloudflare-static/rocket-loader.min.js" data-cf-settings="93fd9015cca482e27f619fc4-|49" defer=""></script></body>
+</html>
