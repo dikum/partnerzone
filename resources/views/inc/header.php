eb95cd905ba0c800e4142bcb7bcc1fdf47799638
dikum <amukida@gmail.com>
2020-06-23 23:44:46 +0100

    Create header.php

---
+++ resources/views/inc/header.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>@yield('title')</title>
+
+
+<!--[if lt IE 10]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="description" content="Admindek Bootstrap admin template made using Bootstrap 4 and it has huge amount of ready made feature, UI components, pages which completely fulfills any dashboard needs." />
+<meta name="keywords" content="flat ui, admin Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
+<meta name="author" content="colorlib" />
+
+<link rel="icon" href="./assets/images/favicon.ico" type="image/x-icon">
+
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Quicksand:500,700" rel="stylesheet">
+
+<link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="./assets/css/waves.min.css" type="text/css" media="all">
+
+<link rel="stylesheet" type="text/css" href="./assets/css/feather.css">
+
+<link rel="stylesheet" type="text/css" href="./assets/css/font-awesome-n.min.css">
+
+<link rel="stylesheet" href="./assets/css/chartist.css" type="text/css" media="all">
+
+<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/widget.css">
+
+<link rel="stylesheet" type="text/css" href="./assets/css/custom.css">
+</head>
+<body>
+
+<div class="loader-bg">
+<div class="loader-bar"></div>
+</div>
+
+<div id="pcoded" class="pcoded">
+<div class="pcoded-overlay-box"></div>
+<div class="pcoded-container navbar-wrapper">
+
+<nav class="navbar header-navbar pcoded-header">
+<div class="navbar-wrapper">
+<div class="navbar-logo">
+<a href="index.html">
+<img class="img-fluid" src="./assets/images/logo.png" alt="Theme-Logo" />
+</a>
+<a class="mobile-menu" id="mobile-collapse" href="#!">
+<i class="feather icon-menu icon-toggle-right"></i>
+</a>
+<a class="mobile-options waves-effect waves-light">
+<i class="feather icon-more-horizontal"></i>
+</a>
+</div>
+<div class="navbar-container container-fluid">
+<ul class="nav-left">
+<li>
+<a href="#!" onclick="if (!window.__cfRLUnblockHandlers) return false; javascript:toggleFullScreen()" class="waves-effect waves-light" data-cf-modified-93fd9015cca482e27f619fc4-="">
+<i class="full-screen feather icon-maximize"></i>
+</a>
+</li>
+</ul>
+<ul class="nav-right">
+<li class="user-profile header-notification">
+<div class="dropdown-primary dropdown">
+<div class="dropdown-toggle" data-toggle="dropdown">
+<span>John Doe</span>
+<i class="feather icon-chevron-down"></i>
+</div>
+<ul class="show-notification profile-notification dropdown-menu" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
+<li>
+<a href="auth-sign-in-social.html">
+<i class="feather icon-log-out"></i> Logout
+</a>
+</li>
+</ul>
+</div>
+</li>
+</ul>
+</div>
+</div>
+</nav>
