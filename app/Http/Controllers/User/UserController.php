033526cf8402a30ded663abbc7475d8223ac5760
dikum <amukida@gmail.com>
2020-07-09 20:05:50 +0100

    Update UserController.php
    Use session in place of cookies

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -2,16 +2,65 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Http\Controllers\Authentication\TokenController;
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Auth\AuthenticationException;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
+
+	public function __construct(){
+		$this->middleware('validate.token:')->except(['isUserLoggedIn']);
+
+	}
+
     public static function isUserLoggedIn(){
-    	if(Cookie::get('user'))
+    	if(session()->has('token') && session()->has('user'))
     		return true;
-    	
-    	return redirect('/user-login');
+    	return false;
+    }
+
+    public function getUser($user){
+
+    	$tokenController = new TokenController();
+    	$tokenController->validateToken();
+
+    	$client = new \GuzzleHttp\Client(['headers' => ['Authorization' => 'Bearer ' . $tokenController->getUserToken()]]);
+
+    	try{
+			$response = $client->request('GET', 'http://partnersoft.test/users/'.$user);
+		}
+		catch(BadResponseException $e){
+			Log::error($e->getResponse()->getBody(true)->getContents());
+			return response()->json(['message' => 'The server encountered an error'], 400);
+		}
+		catch(ClientException $e){
+			Log::error($e->getResponse()->getBody(true)->getContents());
+			return response()->json(['message' => 'There was an error. Please try again.'], 400);
+		}
+		catch(RequestException $e){
+			Log::error($e->getResponse()->getBody(true)->getContents());
+			return response()->json(['message' => 'Error. Please check your connection']);
+		}
+		catch(AuthenticationException $e){
+			//$tokenController->refreshToken();
+			//getUser($user);
+		}
+		catch(Exception $e){
+			Log::error($e->getResponse()->getBody(true)->getContents());
+			return response()->json(['message' => 'There was an error. Please try again.']);
+		}
+
+		if($response->getStatusCode() == 200)
+			return json_decode($response->getBody()->getContents());
+		
+		return response(['message' => 'Could not retrieve user information']);
     }
 }
