715621930c942dbb87d8ab484d1288475cf9b057
dikum <amukida@gmail.com>
2020-07-09 20:07:24 +0100

    Create constants.php

---
+++ config/constants.php
@@ -0,0 +1,5 @@
+<?php
+define('CLIENT_SECRET', 'UgNlFTi3hnvtzJrEWW7x5PtlKBtk8R6zAzmFalOr');
+define('CLIENT_ID', 5);
+define('PASSWORD_CLIENT_SECRET', 'hqPRaHSYN0Q6AWWnZJyAWvtzWwi8hLW9EYWLv0nl');
+define('PASSWORD_CLIENT_ID', 6);
