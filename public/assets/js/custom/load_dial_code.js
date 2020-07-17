587c6b61cbb0e1266dec3f193f787bd9c999c3a3
dikum <amukida@gmail.com>
2020-07-17 02:16:13 +0100

    Create load_dial_code.js
    Load dial code for a country.

---
+++ public/assets/js/custom/load_dial_code.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	//var dial_code;
+	$(document).on('change', '#countryOfResidence', function(event){
+		var selectedCountry = $(this).val();
+
+		for (var i = 0; i<countries.length; i++) {
+			if(countries[i]['countryIdentifier'] == selectedCountry){
+				dial_code = countries[i]['countryDialingCode'];
+				$('#primaryPhoneDialCode').html(dial_code);
+			}
+		}
+	});
+
+
+	$(document).on('change', '#same-dial-code', function(event){
+
+		if(this.checked){
+
+			$('#secondaryPhoneNumber').val($('#primaryPhoneDialCode').text() + $('#secondaryPhoneNumber').val());
+		}
+		else{
+
+			var phoneNumber = $('#secondaryPhoneNumber').val();
+
+			$('#secondaryPhoneNumber').val(phoneNumber.substring($('#primaryPhoneDialCode').text().length, phoneNumber.length));
+		}
+
+	})
+});
