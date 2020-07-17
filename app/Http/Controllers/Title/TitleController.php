18d61ea563ad7b18583403a2b4cc0cdd97759816
dikum <amukida@gmail.com>
2020-07-17 02:13:28 +0100

    Create TitleController.php

---
+++ app/Http/Controllers/Title/TitleController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Title;
+
+use App\Http\Controllers\Authentication\TokenController;
+use App\Http\Controllers\Controller;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class TitleController extends Controller
+{
+    public function index(){
+
+    	$tokenController = new TokenController();
+    	$tokenController->validateToken();
+
+    	$client = new \GuzzleHttp\Client(['headers' => ['Authorization' => 'Bearer ' . $tokenController->getUserToken()]]);
+
+    	try{
+			$response = $client->request('GET', 'http://partnersoft.test/titles/');
+		}
+		catch(BadResponseException $e){
+			Log::error($e->getResponse()->getBody(true)->getContents());
+			return response()->json(['message' => 'The server encountered an error'], 400);
+		}
+		catch(ClientException $e){
+			Log::error($e->getResponse()->getBody(true)->getContents());
+			return response()->json(['message' => 'There was an error. Please try again.'], 400);
+		}
+		catch(RequestException $e){
+			Log::error($e->getResponse()->getBody(true)->getContents());
+			return response()->json(['message' => 'Error. Please check your connection']);
+		}
+		catch(Exception $e){
+			Log::error($e->getResponse()->getBody(true)->getContents());
+			return response()->json(['message' => 'There was an error. Please try again.']);
+		}
+
+		if($response->getStatusCode() == 200)
+			return json_decode($response->getBody()->getContents(), true)['data'];
+
+
+		if($response->getStatusCode() == 401)
+			return response()->json(['message' => 'Sorry, you are not authorized to view this page']);
+
+		
+		return response()->json(['message' => 'Could not retrieve user information']);
+    }
+}
