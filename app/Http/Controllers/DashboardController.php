b3f103f9adcf84f1e4e9ee02a2f9561ebe6f57a1
dikum <amukida@gmail.com>
2020-07-17 02:13:07 +0100

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Authentication\TokenController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cookie;
-use App\Http\Controllers\User\UserController;
+use Illuminate\Support\Facades\Log;
 
 class DashboardController extends Controller
 {
@@ -13,7 +15,11 @@ class DashboardController extends Controller
     	if(UserController::isUserLoggedIn())
     	{
     		$userController =  new UserController();
-    		$user = $userController->getUser(session('user'));
+    		$user = $userController->getUser(session('user')['user_id']);
+
+            if(isset($user->original['message']))
+                return 'Could not load dashboard. Please contact your administrator';
+
     		return view('dashboard', ['user' => explode(' ', $user->data->fullname)[0]]);
     	}
     	
