bb74bd6d2a1489d614d2b11a3dd2accb5fcfce08
dikum <amukida@gmail.com>
2020-07-17 02:15:09 +0100

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,5 +1,51 @@
 <?php
-define('CLIENT_SECRET', 'UgNlFTi3hnvtzJrEWW7x5PtlKBtk8R6zAzmFalOr');
-define('CLIENT_ID', 5);
-define('PASSWORD_CLIENT_SECRET', 'hqPRaHSYN0Q6AWWnZJyAWvtzWwi8hLW9EYWLv0nl');
-define('PASSWORD_CLIENT_ID', 6);
+return [
+
+	'oauth' => [
+		'CLIENT_ID' => 1,
+		'CLIENT_SECRET' => '4uwBOX8HAh6fonFPaRrHXx8z76S9WmZOVLIUgq3D',
+		'PASSWORD_CLIENT_ID' => 2,
+		'PASSWORD_CLIENT_SECRET' => 'yIJ1dGkdpIuzaPTwhdDrVA8u5MNiPMXxQYl15vZl'
+	],
+
+	'calendar' => [
+		'MONTHS' => array(
+		'1' => 'January',
+		'2' => 'February',
+		'3' => 'March',
+		'4' => 'April',
+		'5' => 'May',
+		'6' => 'June',
+		'7' => 'July',
+		'8' => 'August',
+		'9' => 'September',
+		'10' => 'October',
+		'11' => 'November',
+		'12' =>'December'
+		),
+	],
+
+	'marital_status' => [
+		'SINGLE' => 'single',
+		'MARRIED' => 'married',
+		'DIVORCED' => 'divorced'
+	],
+
+	'genders' => [
+		'MALE' => 'male',
+		'FEMALE' => 'female'
+	],
+
+	'preferred_languages' => [
+		'english' => 'english',
+		'spanish' => 'spanish',
+		'french' => 'french',
+		'portuguese' => 'portuguese'
+	],
+
+	'branches' => [
+		'lagos' => 'Lagos',
+		'south africa' => 'south africa',
+		'ghana' => 'ghana'
+	]
+];
