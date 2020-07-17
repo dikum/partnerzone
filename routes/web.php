5a257f21d28d18391734eab0fe770c04994c6b0d
dikum <amukida@gmail.com>
2020-07-17 02:20:08 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,18 @@ Route::get('/logout', 'Authentication\LoginController@logout');
 
 
 Route::get('/partners', 'Partner\PartnerController@index');
+Route::get('/show-partner', 'Partner\PartnerController@show');
 Route::post('/search-partner', 'Partner\PartnerController@search');
-
+Route::post('/update-partner', 'Partner\PartnerController@update');
+Route::get('/delete-partner/{partner}', 'Partner\PartnerController@delete');
+Route::get('/partner-payments/{partner}', 'Partner\PartnerController@payments');
 
 
 Route::get('/countries', 'Country\CountryController@index');
 
 Route::get('/states', 'State\StateController@index');
 
+Route::get('/currencies', 'Currency\CurrencyController@index');
 
 
 
