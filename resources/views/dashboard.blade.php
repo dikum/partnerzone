6713359c29dec3a9458bae98c7642e825f4fb668
dikum <amukida@gmail.com>
2020-08-09 23:35:47 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,26 +3,25 @@
 @section('content')
 
 <div class="pcoded-content">
+	<div class="pcoded-inner-content">
+		<div class="main-body" id="main_body">
+			<div class="page-wrapper">
+				<div class="page-body" id="page-body">
+  					<div class="row">
 
-<div class="pcoded-inner-content">
-<div class="main-body" id="main_body">
-<div class="page-wrapper">
-<div class="page-body">
-  <div class="row">
+  						<div style="margin: 0 auto; text-align: center;">
+    						<div class='time' id="time"></div>
+    						<div class="date" id="date"></div>
 
-  <div style="margin: 0 auto; text-align: center;">
-    <div class='time' id="time"></div>
-    <div class="date" id="date"></div>
-
-    <div class="welcome">
-      Good Morning {{$user}}
-    </div>
-  </div>
-</div>
-</div>
-</div>
-</div>
-</div>
+    						<div class="welcome">
+      							Good Morning {{$user}}
+    						</div>
+  						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <div id="styleSelector">
