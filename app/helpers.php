f522c6fdc973906bf6f35cda5098386b10e9df4b
dikum <amukida@gmail.com>
2020-08-09 23:36:42 +0100

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -3,3 +3,45 @@ function isLoggedInUserAdmin(){
 
 	return session('user')['type'] === 'admin';
 }
+
+function getCurrencyCodeFromCollection($currencies, $currency_id){
+	
+	foreach($currencies as $currency){
+
+		if($currency['currencyIdentifier'] == $currency_id)
+			return $currency['currencyShortName'];
+	}
+
+	return 'Unknown';
+
+}
+
+function getBankNameFromCollection($banks, $bank_id){
+
+	foreach($banks as $bank){
+
+		if($bank['bankIdentifier'] == $bank_id)
+			return $bank['bankName'];
+
+	}
+
+	return 'Unknown';
+
+}
+
+function getCountryNameFromCollection($countries, $country_id){
+
+	foreach($countries as $country){
+
+		if($country['countryIdentifier'] == $country_id)
+			return $country['countryName'];
+
+	}
+
+	return 'Unknown';
+
+}
+
+function getTemplateMessageFromCollection($templates, $template){
+	
+}
