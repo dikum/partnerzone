b902fce696778342506e2cf1119175d0ce9b6029
dikum <amukida@gmail.com>
2020-08-09 23:37:06 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,23 @@ Route::post('/search-partner', 'Partner\PartnerController@search');
 Route::post('/update-partner', 'Partner\PartnerController@update');
 Route::get('/delete-partner/{partner}', 'Partner\PartnerController@delete');
 Route::get('/partner-payments/{partner}', 'Partner\PartnerController@payments');
+Route::get('/partner-create/', 'Partner\PartnerController@create');
+Route::get('/delete-payment/{payment}', 'Payment\PaymentController@delete');
+Route::post('/register-partner/', 'Partner\PartnerController@store');
 
+
+Route::get('/payments', 'Payment\PaymentController@index');
+Route::post('/search-payments', 'Payment\PaymentController@search');
+
+Route::get('/create-message', 'Message\MessageController@create');
+Route::post('/send-message', 'Message\MessageController@send');
+
+
+Route::get('/message-templates', 'Message\MessageController@show_templates');
+Route::get('/message_template-create', 'Message\MessageController@create_template');
+Route::post('/save-message-template', 'Message\MessageController@save_template');
+Route::get('/delete-message-template/{message}', 'Message\MessageController@delete');
+Route::get('/get-template/{template}', 'Message\MessageController@get_template');
 
 Route::get('/countries', 'Country\CountryController@index');
 
