665d1c8233892899f784971495c484c993ddce9c
dikum <amukida@gmail.com>
2020-08-09 23:35:57 +0100

    Update panel.blade.php

--- resources/views/inc/panel.blade.php
+++ resources/views/inc/panel.blade.php
@@ -5,18 +5,56 @@
 				<div class="pcoded-inner-navbar main-menu">
 					<ul class="pcoded-item pcoded-left-item">
 
-						<li class="pcoded-hasmenu active pcoded-trigger">
-							<a href="/" class="waves-effect waves-dark hide-arrow">
+						<li class="pcoded-hasmenu active pcoded-trigger" id="dashboard">
+							<a href="javascript:void(0)" class="waves-effect waves-dark hide-arrow">
 								<span class="pcoded-micon"><i class="feather icon-home"></i></span>
 								<span class="pcoded-mtext">Home</span>
 							</a>
  						</li>
 						<li class="pcoded-hasmenu" id="partner">
 							<a href="javascript:void(0)" class="waves-effect waves-dark hide-arrow">
-								<span class="pcoded-micon"><i class="feather icon-sidebar"></i></span>
-								<span class="pcoded-mtext">Partner</span>
+								<span class="pcoded-micon"><i class="feather icon-users"></i></span>
+								<span class="pcoded-mtext">Partners</span>
 							</a>
 						</li>
+						<li class="pcoded-hasmenu" id="payment">
+							<a href="javascript:void(0)" class="waves-effect waves-dark hide-arrow">
+								<span class="pcoded-micon"><i class="feather icon-credit-card"></i></span>
+								<span class="pcoded-mtext">Payments</span>
+							</a>
+						</li>
+
+						<ul class="pcoded-item pcoded-left-item">
+							<li class="pcoded-hasmenu">
+								<a href="javascript:void(0)" class="waves-effect waves-dark">
+									<span class="pcoded-micon">
+										<i class="feather icon-mail"></i>
+									</span>
+									<span class="pcoded-mtext">Messaging</span>
+								</a>
+								<ul class="pcoded-submenu">
+									<li class="" id="show-send-message">
+										<a href="javascript:void(0)" class="waves-effect waves-dark">
+											<span><i class="fa fa-paper-plane"></i> Send</span>
+										</a>
+									</li>
+
+									<li class="">
+										<a href="javascript:void(0)" class="waves-effect waves-dark">
+											<span><i class="fa fa-history"></i> Log</span>
+										</a>
+									</li>
+
+
+									<li class="" id="message_template">
+										<a href="javascript:void(0)" class="waves-effect waves-dark">
+											<span><i class="fa fa-list"></i> Templates</span>
+										</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+
 					</ul>
 				</ul>
 			</div>
