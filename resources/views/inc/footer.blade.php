53f0af6ac27fcf4ed5509cf1afab344b1825c78c
dikum <amukida@gmail.com>
2021-02-21 12:14:56 +0000

    Update footer.blade.php

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -46,12 +46,19 @@
 <script type="93fd9015cca482e27f619fc4-text/javascript" src="./assets/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script type="text/javascript" src="{{'assets/js/jquery.validate.min.js'}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/additional-methods.min.js" integrity="sha512-6Uv+497AWTmj/6V14BsQioPrm3kgwmK9HYIyWP+vClykX52b0zrDGP7lajZoIY1nNlX4oQuh7zsGjmF7D0VZYA==" crossorigin="anonymous"></script>
 <script data-cfasync="false" src="./assets/js/email-decode.min.js"></script>
 <!--<script type="93fd9015cca482e27f619fc4-text/javascript" src="./assets/js/jquery.min.js"></script>-->
 <script type="93fd9015cca482e27f619fc4-text/javascript" src="./assets/js/jquery-ui.min.js"></script>
 <script type="93fd9015cca482e27f619fc4-text/javascript" src="./assets/js/popper.min.js"></script>
 
-<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js" type="text/javascript"></script>
+<!--<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js" type="text/javascript"></script> -->
+<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js" type="text/javascript"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js" type="text/javascript"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js" type="text/javascript"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.html5.min.js" type="text/javascript"></script>
 
 <script src="./assets/js/waves.min.js" type="93fd9015cca482e27f619fc4-text/javascript"></script>
 
@@ -91,10 +98,20 @@
 <script type="text/javascript" src="./assets/js/custom/partner/register_partner.js"></script>
 <script type="text/javascript" src="./assets/js/custom/payment/payment_dialog.js"></script>
 <script type="text/javascript" src="./assets/js/custom/payment/search_payment.js"></script>
+<script type="text/javascript" src="./assets/js/custom/payment/enter_payment.js"></script>
 <script type="text/javascript" src="./assets/js/custom/message/search_message_partner.js"></script>
 <script type="text/javascript" src="./assets/js/custom/message/message_template.js"></script>
 <script type="text/javascript" src="./assets/js/custom/message/send_message.js"></script>
+<script type="text/javascript" src="./assets/js/custom/message/message_log_search.js"></script>
 <script type="text/javascript" src="./assets/js/custom/toggler.js"></script>
+<script src="https://js.pusher.com/7.0/pusher.min.js"></script>
+
+<script type="text/javascript" src="./assets/js/custom/message/cancel_message.js"></script>
+<script type="text/javascript" src="./assets/js/custom/message/log_dialog.js"></script>
+<!--<script type="text/javascript" src="./assets/js/custom/notification/notification_checker.js"></script>-->
+<script type="text/javascript" src="./assets/js/custom/statement/statement_dialog.js"></script>
+<script type="text/javascript" src="./assets/js/custom/statement/search_statement.js"></script>
+<script type="text/javascript" src="./assets/js/custom/statement/import_statement.js"></script>
 
 
 <script type="text/javascript" src="./assets/js/windowjs/window.min.js"></script>
@@ -121,6 +138,70 @@
         //checks: {xhr: {url: 'https://google.com/'}}
 
     }
+</script>
+
+<!-- Listen to message notifications -->
+<script type="text/javascript">
+
+    var user_id = "<?php echo session('user')['user_id']; ?>";
+
+    Pusher.logToConsole = true;
+    var pusher = new Pusher('1153595b89e97452da79', {
+      cluster: 'ap2'
+    });
+
+    var channel = pusher.subscribe('partnerzone');
+
+    channel.bind('message_progress', function(data) {
+        //$('#notification-list').find('#bulk_message_notification').css('display', 'block');
+        if($('#bulk_message_notification').hasClass('hidden-notification')){
+
+            $('#bulk_message_notification').removeClass('hidden-notification');
+            $('#bulk_message_notification').css('display', 'block');
+
+        }
+
+        if(user_id == data.user_id){
+            var sent = data.progress['success'];
+            var total = data.progress['total'];
+            var message_group = data.message_group;
+            var fails = data.progress['fails'].length;
+            var job_identifier = data.message_job_identifier;
+
+            if(message_group == 'partner_email_batch'){
+                if(total == sent)
+                    $("#bulk_message_notification").find( ".partner-batch-email" ).html(sent + ' batch(es) sent of ' + total + " |<span style ='color=#EE0000;'> " + fails + " failed </span> <label class='label label-success'> Completed</label>");
+                else
+                    $("#bulk_message_notification").find( ".partner-batch-email" ).html(sent + ' batch sent of ' + total + " |<span style ='color=#EE0000;'> " + fails + " failed </span> <button id='"+job_identifier+"' onclick=cancel_message_sending('" + job_identifier + "') class='btn btn-sm btn-danger cancel_button'>Cancel</button>");
+            }
+
+            else if(message_group == 'partner_sms'){
+                if(total == sent)
+                    $("#bulk_message_notification").find( ".partner-sms" ).html(sent + ' Partner SMS sent of ' + total + " |<span style ='color=#EE0000;'> " + fails + " failed </span> <label class='label label-success'> Completed</label>");
+                else
+                    $("#bulk_message_notification").find( ".partner-sms" ).html(sent + ' Partner SMS sent of ' + total + " |<span style ='color=#EE0000;'> " + fails + " failed </span>");
+            }
+
+            else if(message_group == 'list_email_batch'){
+                if(total == sent)
+                    $("#bulk_message_notification").find( ".list-email-batch" ).html(sent + ' batch(es) sent of ' + total + " |<span style ='color=#EE0000;'> " + fails + " failed </span> <label class='label label-success'> Completed</label>");
+                else
+                    $("#bulk_message_notification").find( ".list-email-batch" ).html(sent + ' batch(es) sent of ' + total + " |<span style ='color=#EE0000;'> " + fails + " failed </span>");
+            }
+
+            else if(message_group == 'list_sms'){
+                if(total == sent)
+                    $("#bulk_message_notification").find( ".list-sms" ).html(sent + ' List SMS sent of ' + total + " |<span style ='color=#EE0000;'> " + fails + " failed </span> <label class='label label-success'> Completed</label>");
+                else
+                    $("#bulk_message_notification").find( ".list-sms" ).html(sent + ' List SMS sent of ' + total + " |<span style ='color=#EE0000;'> " + fails + " failed </span>");
+            }
+
+            else
+                $("#bulk_message_notification .notification-time").html("<button onclick=cancel_message_sending('" + job_identifier + "') class='btn btn-sm btn-danger cancel_button'>Cancel</button>");
+        }
+ 
+    });
+
 </script>
 
 </body>
