4d88920d7a1f6e0247a07ef3eb7e73d6c6ebc445
dikum <amukida@gmail.com>
2021-02-21 12:16:26 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -34,9 +34,16 @@ Route::post('/register-partner/', 'Partner\PartnerController@store');
 
 Route::get('/payments', 'Payment\PaymentController@index');
 Route::post('/search-payments', 'Payment\PaymentController@search');
+Route::post('/enter-sr-payment', 'Payment\PaymentController@save_strong_room_payment');
 
 Route::get('/create-message', 'Message\MessageController@create');
-Route::post('/send-message', 'Message\MessageController@send');
+Route::post('/send-bulk-message', 'Message\MessageController@send_bulk');
+//Route::get('/cancel_message_sending', 'Message\MessageController@cancel_job');
+
+
+Route::get('/show-statement', 'BankStatement\BankStatementController@show');
+Route::post('/import-statement', 'BankStatement\BankStatementController@import_bank_statement');
+Route::post('/search-statement', 'BankStatement\BankStatementController@search');
 
 
 Route::get('/message-templates', 'Message\MessageController@show_templates');
@@ -44,6 +51,9 @@ Route::get('/message_template-create', 'Message\MessageController@create_templat
 Route::post('/save-message-template', 'Message\MessageController@save_template');
 Route::get('/delete-message-template/{message}', 'Message\MessageController@delete');
 Route::get('/get-template/{template}', 'Message\MessageController@get_template');
+Route::get('/message-log', 'Message\MessageController@message_log');
+Route::post('/search-message-log', 'Message\MessageController@search');
+
 
 Route::get('/countries', 'Country\CountryController@index');
 
@@ -51,7 +61,7 @@ Route::get('/states', 'State\StateController@index');
 
 Route::get('/currencies', 'Currency\CurrencyController@index');
 
-
+Route::get('/get-new-notifications', 'Notification\NotificationController@get_new_notifications');
 
 Route::get('/test', 'Authentication\LoginController@test');
 
