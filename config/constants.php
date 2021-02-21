0a134cb3c2e821c78368db81061ed3136bf6225d
dikum <amukida@gmail.com>
2021-02-21 12:07:09 +0000

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -49,7 +49,29 @@ return [
 		'ghana' => 'ghana'
 	],
 
-	'api' => 'http://partnersoft.test'
+	'api' => 'http://partnersoft.test',
+
+	'message_settings' => [
+
+		'vas2nets_username' => 'dev@emmanuel.tv',
+		'vas2nets_password' => 'em@2018_182*1',
+		'vas2nets_url' => 'http://www.v2nmobile.com/api/httpsms.php?u=dev@emmanuel.tv&p=em@2018_182*1&m=[MESSAGE]&r=[RECIPIENT]&s=[SENDER]&t=1',
+
+		'bbn_username' => 'dev@emmanuel.tv',
+		'bbn_password' => 'EmmTech2018',
+
+		'twilio_account_sid' => 'ACa7130be977833cecfb8b018d4f9fe25b',
+		'twilio_account_token' => 'e6cf7b0f1ce4db9995526a4ebc6219ae',
+		'sms_sender' => 'Emmanuel TV'
+	],
+
+	'mailgun' => [
+
+		'secret' => env('MAILGUN_SECRET'),
+		'base_url' => env('MAILGUN_API_BASE_URL'),
+		'mail_from' => env('MAIL_FROM_ADDRESS'),
+		'domain' => env('MAILGUN_DOMAIN')
+	],
 
 
 ];
