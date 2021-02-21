f08e8258e2d462f890ab5734731d60423bef92cd
dikum <amukida@gmail.com>
2021-02-21 12:16:11 +0000

    Update search_payment_result.blade.php

--- resources/views/payment/search_payment_result.blade.php
+++ resources/views/payment/search_payment_result.blade.php
@@ -1,35 +1,37 @@
+
 <table class='table table-hover table table-striped table-bordered table-sm' id='paymentTable'>
-    <tbody>
+    <thead class="thead-dark">
         <tr>
+            <th>Partner ID</th>
             <th>Pay Date</th>
             <th>Entered Date</th>
             <th>Amount</th>
             <th>Bank</th>
-            <th>Depositor</th>
+            <th class="hide-column">Depositor</th>
             <th>Entered By</th>
             <th>Channel</th>
-            <th>Description</th>
-            <th>Partner ID</th>
-            <th>Email</th>
-            <th>Phone</th>
+            <th class="hide-column">Description</th>
+            <th class="hide-column">Email</th>
+            <th class="hide-column">Phone</th>
             <th>Action</th>
         </tr>
-
-        @php $row_number = 1; @endphp
-        @foreach($payments as $payment)
-
-            <tr>
-                <td>{{$payment['valueDate']}}</td>
+    </thead>
+        <tbody>
+            @php $row_number = 1; @endphp
+            @foreach($payments as $payment)
+        
+           <tr class="payment-row">
+                <td>{{$payment['friendlyPartnerIdentifier']}}</td>
+                <td>{{$payment['datePaid']}}</td>
                 <td>{{$payment['createdDate']}}</td>
                 <td>{{getCurrencyCodeFromCollection($currencies, $payment['currencyIdentifier'])}} {{$payment['amountPaid']}}</td>
                 <td>{{getBankNameFromCollection($banks, $payment['bankIdentifier'])}}</td>
-                <td>{{$payment['depositor']}}</td>
+                <td class="hide-column payment-depositor">{{$payment['depositor']}}</td>
                 <td>{{$payment['userIdentifier']}}</td>
                 <td>{{$payment['paymentChannel']}}</td>
-                <td>{{$payment['paymentDescription']}}</td>
-                <td>{{$payment['friendlyPartnerIdentifier']}}</td>
-                <td>{{$payment['emailAddress']}}</td>
-                <td>{{$payment['phoneNumber']}}</td>
+                <td class="hide-column payment-description">{{$payment['paymentDescription']}}</td>
+                <td class="hide-column payment-email">{{$payment['emailAddress']}}</td>
+                <td class="hide-column payment-phone">{{$payment['phoneNumber']}}</td>
                 <td>
 
                     @if(getBankNameFromCollection($banks, $payment['bankIdentifier'])  == 'strongroom')<a href='#' onclick="modifyPayment('<?php echo $payment['paymentIdentifier'];?>')" > <i class='fa fa-edit fa-2x' style='color:#52B788; margin-right: 10px;'></i></a> @endif
