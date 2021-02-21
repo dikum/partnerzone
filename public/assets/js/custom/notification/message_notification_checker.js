a63b81048d9c278e90dff208d283c077cb33aa18
dikum <amukida@gmail.com>
2021-02-21 12:08:28 +0000

    Create message_notification_checker.js

---
+++ public/assets/js/custom/notification/message_notification_checker.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    //Pusher.logToConsole = true;
+
+    /*var pusher = new Pusher('1153595b89e97452da79', {
+      cluster: 'ap2'
+    });
+
+    var channel = pusher.subscribe('partnerzone');
+
+    channel.bind('message_progress', function(data) {
+
+      $("#"+data.not_id).find( ".notification-msg" ).html(data.progress['success'] + data.progress['fails'].length + ' sent of ' + data.progress['total']);
+
+    });
+    */
+
+})
